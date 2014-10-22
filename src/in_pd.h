7df91d9681a0c8b22ce5c8aa9e28f33317f64508
0miwi0 <pob.dod@gmail.com>
2014-10-22 01:35:06 -0400

    Code::Blocks Project

---
+++ src/in_pd.h
@@ -0,0 +1,29 @@
+
+#pragma once
+
+#include <windows.h>
+#include "PdBase.hpp"
+#include "winamp.h"
+
+#define WM_WA_MPEG_EOF WM_USER+2
+#define NCH 2
+#define SAMPLERATE 44100
+#define BPS 16
+#define ticks 8
+
+using namespace pd;
+
+PdBase libpd;
+Patch p;
+
+char lastfn[MAX_PATH];	// currently playing file
+
+int file_length;// file length, in bytes
+int pos;		// time in milliseconds.
+int paused;		// are we paused?
+
+short* inBuf; // interleaved input audio buffer
+short* outBuf;
+
+volatile int killDecodeThread=0;			// the kill switch for the decode thread
+HANDLE thread_handle=INVALID_HANDLE_VALUE;	// the handle to the decode thread
