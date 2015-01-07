c0bf484f9525ed57d67d14cd11f96add4b10d2ec
0miwi0 <pob.dod@gmail.com>
2015-01-07 00:06:26 -0500

    new object

--- workspace/amPd/libpd/pure-data/extra/miwi/miwi.c
+++ workspace/amPd/libpd/pure-data/extra/miwi/miwi.c
@@ -1,14 +1,16 @@
+void graid_setup(void);
+void grand_setup(void);
 void rand_setup(void);
 void randv_setup(void);
-void grand_setup(void);
-void graid_setup(void);
+void rind_setup(void);
 void muse_setup(void);
 void miwi_setup(void);
 
 void miwi_setup(void) {
+	graid_setup();
+	grand_setup();
 	rand_setup();
 	randv_setup();
-	grand_setup();
-	graid_setup();
+	rind_setup();
 	muse_setup();
 }
